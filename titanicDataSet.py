3b958260564bdbfe229d6187039538bb482c6828
Matteo Staar <matteo.staar@gmx.de>
2026-03-31 20:45:48 +0200

    added neurons/layers

--- titanicDataSet.py
+++ titanicDataSet.py
@@ -109,13 +109,22 @@ class TitanicNet(nn.Module):
 
         self.fc1 = nn.Linear(input_dim, 3)
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(3, 1)
+        self.fc2 = nn.Linear(3, 32)
+        self.fc3 = nn.Linear(32, 64)
+        self.fc4 = nn.Linear(64, 32)
+        self.fc5 = nn.Linear(32, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
+        x = self.relu(x)
+        x = self.fc5(x)
         x = self.sigmoid(x)
         return x
 
