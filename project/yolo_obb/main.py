52a318ca21a3353258f3b768b1152fc28bd1f23b
kienda <83455653+kida31@users.noreply.github.com>
2026-06-09 21:08:59 +0200

    add train results (includes some v1.5)

--- project/yolo_obb/main.py
+++ project/yolo_obb/main.py
@@ -4,8 +4,8 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     freeze_support()
     model = YOLO('yolov8n-obb.yaml')
-    results = model.train(data="DOTAv1.yaml",
-                          time=0.5,
+    results = model.train(data="DOTAv1.5.yaml",
+                          time=.5,
                           imgsz=1024,
                           batch=-1,
                           pretrained=False,
